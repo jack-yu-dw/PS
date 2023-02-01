38da186a426c2ed983e0dab940fc4f073fbff714
jack-yu-dw <dongwook1269@gmail.com>
2023-02-01 14:24:37 +0900

    이코테 유형별 기출 - 다이나믹 프로그래밍

---
+++ DP/이코테/dpP2.py
@@ -0,0 +1,24 @@
+# p.376 정수 삼각형
+# https://www.acmicpc.net/problem/1932
+
+n = int(input())
+dp = [[] for _ in range(n)]
+for i in range(n):
+    data = list(map(int, input().split()))
+    dp[i] = data
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            left = 0
+            right = dp[i-1][0]
+        elif j == i:
+            left = dp[i-1][j-1]
+            right = 0
+        else:
+            left = dp[i-1][j-1]
+            right = dp[i-1][j]
+        dp[i][j] = dp[i][j] + max(left, right)
+
+result = dp[n-1][:]
+print(max(result))
